f5321b01223633a95992f19b48b3530634170c2a
vian-ai <92945218+vian-ai@users.noreply.github.com>
2023-03-15 16:01:14 -0600

    add function to generate a random password

---
+++ genpw.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+charset="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~!?@#$%^&*<>"
+length=$1
+rand=$RANDOM
+
+# Function to generate a random password
+function genpw {
+	for (( i=0; i < $length; i++ )); do
+		rand=$RANDOM
+		rand=$(($rand % ${#charset}))
+		echo -n ${charset:$rand:1}
+	done
+	echo
+}
+
+# Invoking Functions
+genpw $length
