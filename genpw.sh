06cd6ad5ebdb6d284821a5fcbb9cf6bad70ce45e
vian-ai <92945218+vian-ai@users.noreply.github.com>
2023-03-15 16:10:01 -0600

    edit generator function to reduce char repitition

--- genpw.sh
+++ genpw.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 
-charset="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~!?@#$%^&*<>"
+char_set="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?@&"
 length=$1
+prev_char=""
 
 # Function to generate a random password
 function genpw {
-	for (( i=0; i < $length; i++ )); do
-		rand=$RANDOM
-		rand=$(($rand % ${#charset}))
-		echo -n ${charset:$rand:1}
-	done
-	echo
+    for (( i=0; i < $length; i++ )); do
+        # Generate a random index into the charset
+        rand=$RANDOM
+        rand=$(($rand % ${#char_set}))
+        char=${char_set:$rand:1}
+        
+        # Check if the previous character is the same as the current one
+        if [ "$char" == "$prev_char" ]; then
+            # Generate a new character until it's different from the previous one
+            while [ "$char" == "$prev_char" ]; do
+                rand=$RANDOM
+                rand=$(($rand % ${#char_set}))
+                char=${char_set:$rand:1}
+            done
+        fi
+        
+        # Print the current character and update the previous character
+        echo -n $char
+        prev_char=$char
+    done
+    echo
 }
 
 # Invoking Functions
