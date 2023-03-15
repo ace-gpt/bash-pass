71d4f948460b2bcca23ef59bf2cb6c2aa7b17ce2
vian-ai <92945218+vian-ai@users.noreply.github.com>
2023-03-15 16:51:27 -0600

    refactor generator

--- genpw.sh
+++ genpw.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-char_set="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?@&"
+# Charset for password
+charset="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?@&"
+# Get the length of the password
 length=$1
-prev_char=""
 
-# Check if the length of the password is specified
+# Check if the length is specified
 if [ -z "$length" ]; then
     echo "Usage: genpw.sh <length>"
     exit 1
@@ -16,30 +17,28 @@ sleep 1
 # Function to generate a random password
 function genpw {
     for (( i=0; i < $length; i++ )); do
-        # Generate a random index into the charset
-        rand=$RANDOM
-        rand=$(($rand % ${#char_set}))
-        char=${char_set:$rand:1}
-        
-        # Check if the previous character is the same as the current one
-        if [ "$char" == "$prev_char" ]; then
-            # Generate a new character until it's different from the previous one
-            while [ "$char" == "$prev_char" ]; do
-                rand=$RANDOM
-                rand=$(($rand % ${#char_set}))
-                char=${char_set:$rand:1}
-            done
-        fi
-        
-        # Print the current character and update the previous character
-        echo -n $char
-        prev_char=$char
+    # Generate a random character
+    rand=$(od -vAn -N4 -tu4 < /dev/urandom)
+    rand=$(($rand % ${#charset}))
+    char=${charset:$rand:1}
+
+    # Check if previous char is the same as the current char
+    if [ "$char" == "$prev_char" ]; then
+        # Generate a new random character
+        while [ "$char" == "$prev_char" ]; do
+            rand=$(($rand % ${#charset}))
+            char=${charset:$rand:1}
+        done
+    fi
+
+    # Print the character and update the previous character
+    echo -n $char
+    prev_char=$char
     done
-    echo
 }
 
 # Invoking Functions
-password=$(genpw $length)
+password=$(genpw)
 
 # Print the password
 echo "✨ $password"
